2f74009025551c7cb35b082c2b9d85d95f3be8ea
Soban Syeed <Soban.syeed@outlook.com>
2024-12-18 21:49:27 +0530

    Added MainMenu functionality to Main Scene
    Main Menu button takes back to Menu Scene upon clicking

--- Assets/Scripts/MainUIHandler.cs
+++ Assets/Scripts/MainUIHandler.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainUIHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // To go back to Menu Scene
+    public void BackToMenu()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(0);
     }
 }
